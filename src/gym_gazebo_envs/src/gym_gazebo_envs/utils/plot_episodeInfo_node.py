ed0a7c20d482a29183ce40d755e179e49f6c904e
victorfdezc <victorfdezc1996@gmail.com>
2020-11-20 14:42:02 +0100

    added average step time plottinh

--- src/gym_gazebo_envs/src/gym_gazebo_envs/utils/plot_episodeInfo_node.py
+++ src/gym_gazebo_envs/src/gym_gazebo_envs/utils/plot_episodeInfo_node.py
@@ -12,32 +12,31 @@ rospy.init_node('plotEpisodeInfoNode', anonymous=True)
 r = rospy.Rate(1)
 
 # Global variables to store data
-x = np.array([])
-y_r = np.array([])
-y_s = np.array([])
+eps = np.array([])
+rwd = np.array([])
+nstp = np.array([])
+tstp = np.array([])
+ma_rwd = np.array([])
+ma_nstp = np.array([])
+ma_tstp = np.array([])
 data_updated = False
 
-def moving_average(data) : # TODO:change funtion description
-    # TODO: check if we can do an online moving average update for efficiency reasons... Now we are recomputing
-    # the whole moving average each time the function is called
-    '''
-    Estamos interesados en obtener la media de los retornos a lo largo de 100 episodios, ya que,
-    segun la documentacion de OpenAI Gym el agente es juzgado segun el retorno obtenido a lo largo
-    de los 100 episodios.
-    '''
-    N = len(data)
-    moving_average = np.empty(N)
-    for t in range(N):
-        moving_average[t] = data[max(0, t-100):(t+1)].mean()
-    return moving_average
-
 def episodeInfo_callback(data):
-    global y_r, y_s, x, data_updated
+    global eps, rwd, nstp, tstp, ma_rwd, ma_nstp, ma_tstp, data_updated
     # Get data from topic
     if not(data.episode_number == 0 and data.episode_reward == 0):
-        y_r = np.append(y_r, data.episode_reward)
-        y_s = np.append(y_s, data.total_episode_steps)
-        x = np.append(x, data.episode_number)
+        eps = np.append(eps, data.episode_number)
+        rwd = np.append(rwd, data.episode_reward)
+        nstp = np.append(nstp, data.total_episode_steps)
+        tstp = np.append(tstp, data.average_step_time)
+
+        # Compute average taking into account the last 100 episodes
+        # (standard way of knowing the performance of our agent in
+        # OpenAI Gym)
+        ma_rwd = np.append(ma_rwd, rwd[-100:].mean())
+        ma_nstp = np.append(ma_nstp, nstp[-100:].mean())
+        ma_tstp = np.append(ma_tstp, tstp[-100:].mean())
+
         data_updated = True
 
 # Suscribe to "/episode_info" topic:
@@ -51,54 +50,71 @@ fig1 = plt.figure(1)
 fig1.canvas.set_window_title("Episode Rewards")
 fig2 = plt.figure(2)
 fig2.canvas.set_window_title("Episode Steps")
+fig3 = plt.figure(3)
+fig3.canvas.set_window_title("Average Step Time (sec)")
 # Get the axes:
 ax1 = fig1.add_subplot(111)
 ax2 = fig2.add_subplot(111)
+ax3 = fig3.add_subplot(111)
 # x axis will be always a integer data (episode num):
 ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
 ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax3.xaxis.set_major_locator(MaxNLocator(integer=True))
 # Make the first plot:
-line1, = ax1.plot(x, y_r, 'r-') # Returns a tuple of line objects, thus the comma
-line2, = ax1.plot(x, moving_average(y_r), 'b-') # Returns a tuple of line objects, thus the comma
-line3, = ax2.plot(x, y_s, 'g-') # Returns a tuple of line objects, thus the comma
-line4, = ax2.plot(x, moving_average(y_s), 'm-') # Returns a tuple of line objects, thus the comma #TODO:change color!!
+line1, = ax1.plot(eps, rwd, color='red', linestyle='solid') # Returns a tuple of line objects, thus the comma
+line2, = ax1.plot(eps, ma_rwd, color='blue', linestyle='solid')
+line3, = ax2.plot(eps, nstp, color='lime', linestyle='solid')
+line4, = ax2.plot(eps, ma_nstp, color='darkviolet', linestyle='solid')
+line5, = ax3.plot(eps, tstp, color='darkorange', linestyle='solid')
+line6, = ax3.plot(eps, ma_tstp, color='darkgreen', linestyle='solid')
 
 if __name__ == '__main__':
     while not rospy.is_shutdown():
         if data_updated: 
-            # TODO: make a function that updates all the graphs, so we can use that function in the callback to avoid running all time a loop. So that
-            # function must have all the code below to update the graphs (maybe is not a good idea because another callback could arrive
-            # meanwhile we are plotting...)
-
             # Save data to avoid problems if the callback executes meanwhile we are plotting
-            rewards = y_r # TODO: change names to avoid confusion
-            steps = y_s
-            episodes = x
+            rewards = rwd
+            steps = nstp
+            step_time = tstp
+            ma_rewards = ma_rwd
+            ma_nsteps = ma_nstp
+            ma_tstep = ma_tstp
+            episodes = eps
+            
             # Add new data:
             line1.set_ydata(rewards)
             line1.set_xdata(episodes)
-            line2.set_ydata(moving_average(rewards))
+            line2.set_ydata(ma_rewards)
             line2.set_xdata(episodes)
+
             line3.set_ydata(steps)
             line3.set_xdata(episodes)
-            line4.set_ydata(moving_average(steps))
+            line4.set_ydata(ma_nsteps)
             line4.set_xdata(episodes)
+
+            line5.set_ydata(step_time)
+            line5.set_xdata(episodes)
+            line6.set_ydata(ma_tstep)
+            line6.set_xdata(episodes)
             
             data_updated = False
 
             # Make sure all the data fits
             ax1.relim()
             ax2.relim()
+            ax3.relim()
             # Autoscale the plot
             ax1.autoscale()
             ax2.autoscale()
+            ax3.autoscale()
             # And draw
             fig1.canvas.draw()
             fig2.canvas.draw()
+            fig3.canvas.draw()
             # Delete all events
             fig1.canvas.flush_events()
             fig2.canvas.flush_events()
+            fig3.canvas.flush_events()
 
-            rospy.loginfo("Average Reward/Steps for the last 100 episodes: {:0.2f} / {:0.2f}".format(rewards[-100:].mean(),steps[-100:].mean()))
+            rospy.loginfo("\nAverage (last 100 episodes):\n    Rewards: {:0.2f}\n    Steps: {:0.2f}\n    Step Time (sec): {:0.6f}\n---------------".format(ma_rewards[-1],ma_nsteps[-1],ma_tstep[-1]))
 
         r.sleep()
